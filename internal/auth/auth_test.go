53d227af4839b7f62aabc5c9fa87862c1eb07d66
iamwzl <wzl@wzl.to>
2025-03-11 21:08:10 +0000

    Adding forgotten test file

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+
+	t.Error("Intentionally broken test for CH2:L2")
+	return
+
+	// Test 1 - Valid Header
+	headerTest := http.Header{
+		"Authorization": []string{"ApiKey atestkey"},
+	}
+	apiKey, err := GetAPIKey(headerTest)
+
+	if err != nil {
+		t.Errorf("Got an unexpected err: %v", err)
+	}
+	if apiKey != "atestkey" {
+		t.Errorf("Was expecting 'atestkey' got: %v", apiKey)
+	}
+
+	// Test 2 - Invalid header
+	headerTest2 := http.Header{
+		"Authorization": []string{"invalid"},
+	}
+
+	apiKey, err = GetAPIKey(headerTest2)
+	if err == nil {
+		t.Errorf("Was expecting an error, not: %v", apiKey)
+	}
+
+}
